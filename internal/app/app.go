3c5d1eae697fa2382a7c732cece4f97646d49e0c
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2023-09-25 00:21:17 +0600

    new file stature init

---
+++ internal/app/app.go
@@ -0,0 +1,9 @@
+package app
+
+import "log"
+
+func StartApp() {
+
+	log.Print("Hello from app.App()")
+
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/MOHAMMADmiZAN/go_recipe/internal/app"
+
+func main() {
+
+	app.StartApp()
+
+}
